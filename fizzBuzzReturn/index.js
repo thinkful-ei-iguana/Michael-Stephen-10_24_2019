bfbcb0892e5dbe2fa7ba7ff171ecb6f3ff6f418d
Michael Weedman <michael.weedman@gmail.com>
2019-10-24 11:35:04 -0700

    Update index.js
    Got input value logged.

---
+++ fizzBuzzReturn/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$ (function(){
+
+    $('form').submit(e => {
+    //have function that accepts user input
+    event.preventDefault();
+    let inputValue = $(event.currentTarget).find('input').val();
+    console.log(inputValue);
+
+    //put stuff into js-results
+    //.js-results
+    });
+});
