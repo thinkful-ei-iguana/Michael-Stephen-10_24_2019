6fc47ed852c688c800b4e4f5e93b53a9b74f0850
stephenschab <sschab689@gmail.com>
2019-10-24 13:39:24 -0600

    almost done with fizz buzz

--- fizzBuzzReturn/index.js
+++ fizzBuzzReturn/index.js
@@ -2,31 +2,36 @@
 
 $ (function(){
 
-  $('form').submit(e => {
-    //have function that accepts user input
+  $('form').submit(event => {
+    
     event.preventDefault();
-    let inputValue = $(event.currentTarget).find('input').val();
+
+    $('.js-results').empty();
+
+    let inputValue = parseInt($(event.currentTarget).find('input[name = "number-choice"]').val());
 
     //put stuff into js-results
     //.js-results
-    function logRes(input) {
-      for (let i = 0; i <= input; i++) {
-        if(i % 3 === 0 && i % 5 === 0) {
-          $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
-        }
-        else if (i % 5 === 0) {
-          $('.js-results').append('<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
-        }
-        else if (i % 3 === 0) {
-          $('.js-results').append('<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
-        }
-        else {
-          $('.js-result').append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
-        }
+
+    const result = [];
+
+    for (let i = 1; i <= inputValue; i++) {
+      if(i % 3 === 0 && i % 5 === 0) {
+        result.push($('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'));
+      }
+      else if (i % 5 === 0) {
+        result.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
+      }
+      else if (i % 3 === 0) {
+        result.push($('<div class="fizz-buzz-item fizz"><span>fizz</span></div>'));
+      }
+      else {
+        result.push($(`<div class="fizz-buzz-item"><span>${i}</span></div>`));
       }
     }
 
-    logRes(inputValue);
+    $('.js-result').append(result);
+    console.log(result);
 
   });
 });
