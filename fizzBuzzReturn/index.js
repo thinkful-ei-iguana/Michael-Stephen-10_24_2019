c26c04baedcd64c46d79998b7f517eadac97239c
stephenschab <sschab689@gmail.com>
2019-10-24 13:13:08 -0600

    worked on fizz buzz

--- fizzBuzzReturn/index.js
+++ fizzBuzzReturn/index.js
@@ -2,13 +2,31 @@
 
 $ (function(){
 
-    $('form').submit(e => {
+  $('form').submit(e => {
     //have function that accepts user input
     event.preventDefault();
     let inputValue = $(event.currentTarget).find('input').val();
-    console.log(inputValue);
 
     //put stuff into js-results
     //.js-results
-    });
+    function logRes(input) {
+      for (let i = 0; i <= input; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+          $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
+        }
+        else if (i % 5 === 0) {
+          $('.js-results').append('<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
+        }
+        else if (i % 3 === 0) {
+          $('.js-results').append('<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
+        }
+        else {
+          $('.js-result').append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
+        }
+      }
+    }
+
+    logRes(inputValue);
+
+  });
 });
